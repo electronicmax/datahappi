7889c93d10ab7362508968dce36341747e1f22b9
electronic max <electronic@gmail.com>
2012-06-25 17:20:57 +0100

    added comments

--- js/chain-engine.js
+++ js/chain-engine.js
@@ -3,8 +3,10 @@ define([],
            var EQ = function(l1,l2) {
                return l1.length == l2.length && _(l1).difference(l2).length == 0;
            };
+           var flatten = function(L) {
+               return L.reduce(function(x,y) { return x.concat(y); });
+           }           
            var DEFINED = function(x) {  return x !== undefined;    }
-           // 
            var TRANSFORMERS = [
                {
                    domain:["within"],
@@ -32,27 +34,31 @@ define([],
                    fn: function(x) { return { within: { lat: 328, lng : 2398} }; }
                }                              
            ];
-
-           var flatten = function(L) {
-               return L.reduce(function(x,y) { return x.concat(y); });
-           }
-                      
+           
            return {
                find_chain: function(src_type, dest_type, transformers) {
                    var transformers = transformers || TRANSFORMERS;
                    var me = arguments.callee;
+                   
                    if (EQ(src_type, dest_type)) { return [[]]; } // goal achieved.
-                   var applicable = transformers.filter(function(T) {
+
+                   // find transforms that will give us our current destination
+                   var good_destination = transformers.filter(function(T) {
                        return EQ(T.range, dest_type);
-                   });                   
-                   if (applicable.length == 0) { return [undefined]; }
-                   return flatten(applicable.map(function(T) {
-                       var m = me(src_type, T.domain, transformers);
-                       var retval =  m.filter(DEFINED).map(function(trail) {
+                   });
+                   
+                   // if none can be found, fail.
+                   if (good_destination.length == 0) { return [undefined]; }
+
+                   // try to find a path from our src to each candidate's domain
+                   return flatten(good_destination.map(function(T) {
+                       var m = me(src_type, T.domain, transformers); // recurse!
+                       return m.filter(DEFINED).map(function(trail) {
                            return trail.concat(T); 
                        });
-                       return retval;
                    }));
                }
            };
+
+           
        });
