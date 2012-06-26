d0bfd977cb724de02ce436257a0ea18493378bb2
Peter West <peter.g.west@gmail.com>
2012-06-26 13:12:52 +0100

    Fixed proxy server

--- proxy/server.js
+++ proxy/server.js
@@ -1,22 +1,42 @@
 var http = require('http'),
     httpProxy = require('http-proxy'),
-    url  = require('url');;
+    url  = require('url');
 
 httpProxy.createServer(function (req, res, proxy) {
-    var url_parts = url.parse(req.url, true),
+    var header, value,
+        url_parts = url.parse(req.url, true),
         host = url_parts.query.host || "localhost",
-        path = url_parts.pathname;
+        port = url_parts.query.port || 80,
+        path = url_parts.pathname,
+        cors_headers = {
+            'access-control-allow-methods': 'HEAD, POST, GET, PUT, PATCH, DELETE',
+            'access-control-max-age': '86400',
+            'access-control-allow-headers': 'accept, accept-charset, accept-encoding, accept-language, authorization, content-length, content-type, host, origin, proxy-connection, referer, user-agent, x-requested-with',
+            'access-control-allow-credentials': 'true',
+            'access-control-allow-origin': req.headers.origin
+        };
+
+    for (header in req.headers) {
+        cors_headers['access-control-allow-headers'] += ", " + header;
+    }
+    console.log(cors_headers['access-control-allow-headers'])
 
     console.log("================ New request ================");
     console.log(" HOST:     ", host);
     console.log(" PATH NAME:", path);
     console.log(" HEADERS:\n", req.headers);
 
-    req.headers['accept'] = 'application/xhtml+xml,text/html';
+    for (header in cors_headers) {
+        value = cors_headers[header];
+        res.setHeader(header, value);
+    }
+
+    // we want to bring the new host over into the headers
+    req.headers.host = host;
 
     return proxy.proxyRequest(req, res, {
         host: host,
-        port: 80
+        port: port
     });
 
 }).listen(9292);
