48542db26d6f230551d590fb2e8b93f1ec0c9ffe
Thomas Smith <taes1g09@zepler.net>
2012-06-28 15:21:38 +0100

    Added missing modified files from previous commit, also prototype.html.

--- examples/js/map-test.js
+++ examples/js/map-test.js
@@ -1,8 +1,8 @@
 define(['../../js/rdf/RDFCollection',
-        '../../js/widgets/leaflet-map'],
+        '../../js/widgets/leafletMap'],
        function(rdfc, leafletMap) {
            /// put cool shtuff here.
            rdfc.test().then(function(data) { console.log(" omg data ", data); });
-           
+           new leafletMap.LeafletWidget( {el: $('#map'), attributes: {css: {height: "400px"}} })
            return {};
        });
--- js/widgets/leafletMap.js
+++ js/widgets/leafletMap.js
@@ -4,12 +4,8 @@ define([],
                initialize:function() {
                    var this_ = this;
                    // add our required leaflet stylesheet
-                   $('head').append('<link rel="stylesheet" href="../../lib/leaflet/leaflet.css" type="text/css" />');
-                   $.getScript('../../lib/leaflet/leaflet.js').then(
-                       function() {
-                           this_.render();
-                       });
-                   this.options.valuefn = this.options.valuefn || function(x) { return x; };
+                   $('head').append('<link rel="stylesheet" href="../lib/leaflet/leaflet.css" type="text/css" />');
+                   this_.render();
                    this.collection = this.options.data || new Backbone.Collection();
                },
                setCollection:function(c) {
@@ -21,11 +17,17 @@ define([],
                render:function() {
                    // set up the map here.
                    // Assume: this.options.el is the element you want to put the map
-                   var map = new L.Map(this.options.el, this.options.map_options);
-                   var cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/YOUR-API-KEY/997/256/{z}/{x}/{y}.png', {
+                   var map = new L.Map(this.el, this.options.map_options);
+                   var cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/285675b50972436798d67ce55ab7ddde/997/256/{z}/{x}/{y}.png', {
                        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
                        maxZoom: 18
                    });
+                   var markerLocation = new L.LatLng(50.936592, -1.398697);
+                   map.setView(markerLocation, 13).addLayer(cloudmade);
+
+                   var marker = new L.Marker(markerLocation);
+                   map.addLayer(marker);
+                   return this;
                }               
            });           
            return { LeafletWidget:LeafletWidget  };           
