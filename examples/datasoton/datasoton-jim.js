771fd8b94a88e3a1eecf367f68533882fec2dc0e
JimSkinner <js40g09@ecs.soton.ac.uk>
2012-06-29 17:01:45 +0100

    Created folder to hold local RDF documents mostly used in debugging. Managed to get the 'places' dataset read in correctly, but the 'events' dataset isn't parsing.

--- examples/datasoton/datasoton-jim.js
+++ examples/datasoton/datasoton-jim.js
@@ -1,7 +1,21 @@
 define(["./rdfcollection"],
 	function(rdfcollection) {
 		"strict mode";
-		var collection = rdfcollection('http://data.southampton.ac.uk/building/58.rdf');
+		var placesCollection = rdfcollection('http://data.southampton.ac.uk/dumps/places/2012-06-06/places.rdf');
+		var eventsCollection = rdfcollection('http://data.southampton.ac.uk/dumps/events-diary/2012-06-29/events-diary.rdf');
+
+		$.when(
+			placesCollection.fetch(),
+			eventsCollection.fetch()
+		).done(function() {
+			console.log("done");
+			console.log(placesCollection);
+			console.log(eventsCollection);
+		}).fail(function(e) {
+			console.error(e);
+		});
+
+		/*
 		collection.fetch().then(
 			var rdfView = new Backbone.View.extend({
 				el: $("ul#modelList"),
@@ -14,5 +28,6 @@ define(["./rdfcollection"],
 				},
 			});
 		);
+		*/
 	}
 );
