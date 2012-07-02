2e0a0cf69df3a6d047826088126a1f945eb9681e
Peter West <peter.g.west@gmail.com>
2012-07-02 13:13:51 +0100

    Handled connection failure in new-proxy

--- proxy/new-proxy.js
+++ proxy/new-proxy.js
@@ -32,30 +32,37 @@ httpProxy.createServer(function (req, res, proxy) {
     console.log(" HEADERS:\n", req.headers);
 
     var proxy = http.createClient(80, host);
-
+    proxy.on('error', function (e) {
+        console.log("error:", e);
+        console.log("returning -> 404");
+        return res.writeHead(404, {});
+    });
     // we want to bring the new host over into the headers
     req.headers.host = host;
 
-    var proxy_request = proxy.request(req.method, path, req.headers);
-    proxy_request.addListener('response', function (proxy_response) {
-        proxy_response.addListener('data', function(chunk) {
-            res.write(chunk, 'binary');
+    //try {
+        var proxy_request = proxy.request(req.method, path, req.headers);
+        proxy_request.addListener('response', function (proxy_response) {
+            proxy_response.addListener('data', function(chunk) {
+                res.write(chunk, 'binary');
+            });
+            proxy_response.addListener('end', function() {
+                res.end();
+            });
+            var headers = proxy_response.headers;
+            for (name in cors_headers) {
+                headers[name] = cors_headers[name];
+            }
+            res.writeHead(proxy_response.statusCode, proxy_response.headers);
         });
-        proxy_response.addListener('end', function() {
-            res.end();
+        req.addListener('data', function(chunk) {
+            proxy_request.write(chunk, 'binary');
         });
-        var headers = proxy_response.headers;
-        for (name in cors_headers) {
-            headers[name] = cors_headers[name];
-        }
-        res.writeHead(proxy_response.statusCode, proxy_response.headers);
-    });
-    req.addListener('data', function(chunk) {
-        proxy_request.write(chunk, 'binary');
-    });
-    req.addListener('end', function() {
-        proxy_request.end();
-    });
+        req.addListener('end', function() {
+            proxy_request.end();
+        });
+    //} catch (e) {
+    //}
 
     /*
       // abbreviated way >
@@ -68,3 +75,4 @@ httpProxy.createServer(function (req, res, proxy) {
     */
 
 }).listen(9292);
+
