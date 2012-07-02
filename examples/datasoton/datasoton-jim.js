2d66f4e985dacde01d63da2555ba6cf6ac9ddc3e
Jim <js40g09@ecs.soton.ac.uk>
2012-07-02 11:00:19 +0100

    Replaces require.js with non-minimized version. Comitting datasoton-jim so I can pull.

--- examples/datasoton/datasoton-jim.js
+++ examples/datasoton/datasoton-jim.js
@@ -1,18 +1,23 @@
 define(["./rdfcollection"],
 	function(rdfcollection) {
 		"strict mode";
-		var collection = rdfcollection('http://data.southampton.ac.uk/building/58.rdf');
-		collection.fetch().then(
-			var rdfView = new Backbone.View.extend({
+		window.collection = rdfcollection('http://id.southampton.ac.uk/dataset/places/latest.rdf')
+		window.collection = rdfcollection('http://volant.ecs.soton.ac.uk/examples/datasoton/places.rdf')
+		window.collection2 = rdfcollection('http://data.southampton.ac.uk/building/58.rdf');
+		collection.fetch().then(function() {
+			var rdfView = Backbone.View.extend({
 				el: $("ul#modelList"),
 				render: function() {
 					that = this;
-					c.models.map(function(model) {
-						$(that.el).append("<li>"+model.cid+"</li>");
+					collection.models.map(function(model) {
+						window.m = model;
+						$(that.el).append("<li>"+model.attributes._id+"\n"+model.attributes["http://purl.org/dc/terms/spatial"]+"</li>");
 					});
 					return this;
-				},
+				}
 			});
-		);
+			var a = new rdfView();
+			a.render();
+		});
 	}
 );
