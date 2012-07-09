59f9a1747b8dafe3565d57eb3ce49e7e48ccb74c
electronic max <electronic@gmail.com>
2012-07-09 14:46:39 +0100

    added caching support to the operators

--- js/ops/chain-engine.js
+++ js/ops/chain-engine.js
@@ -52,7 +52,6 @@ define([],
                {
                    domain:["http://purl.org/NET/c4dm/event.owl#place"],
                    fn: function(x) {
-                       console.log(' place name ', x, x.get("http://purl.org/NET/c4dm/event.owl#place"));
                        if (x.get("http://purl.org/NET/c4dm/event.owl#place") &&
                            x.get("http://purl.org/NET/c4dm/event.owl#place").filter(function(y) { return y.get('http://www.w3.org/2000/01/rdf-schema#label'); })) {
                            // 
@@ -63,7 +62,6 @@ define([],
                                // then get hte label
                                return y.get('http://www.w3.org/2000/01/rdf-schema#label')[0];
                            });
-                           console.log('labels ', labels);
                            return to_model({ 'place name' : labels[0] });
                        }
                    }
@@ -91,44 +89,80 @@ define([],
                // entity is a Backbone.Model
                // console.log(" tgt type ", tgt_type, " KEYS : ", _(entity.attributes).keys(), _(tgt_type).without(_(entity.attributes).keys()));
                if (! entity instanceof Backbone.Model) {
-                   console.log("Got > ", entity);
+                   console.error("Warning: Not a model, Got > ", entity);
                    return false;
                }
                if (!_(entity.attributes).isObject()) {
-                   console.log("Weird entity : ", entity, entity.attributes);
+                   console.error("Weird entity : ", entity, entity.attributes);
                }
                return _(tgt_type).difference(_(entity.attributes).keys()).length == 0;
            }
 
-	   var caching = { };
-	   // applications of { (model_id) -> ( (T, val1) ... ) ) }
+           // FnCache - maintains a cache of applications of a function
+           // keyed on 2 values: a model, first, and the function, second
+           var FnCache = function() {
+               this.cache = {}; // m_id -> [ (fn, val) ... ]
+               this.models = [];
+           };
+           FnCache.prototype = {
+               get_value:function(fn,m) {
+                   var m_id = m.id;
+                   var apps = this.cache[m_id] && this.cache[m_id].filter(function(fnapp) { return fnapp[0] == fn; });
+                   if (apps && apps.length) {
+                       return apps[0][1];
+                   }
+                   return undefined; // 
+               },
+               set_value:function(fn, m, v) {
+                   // fn is the function to be applied
+                   // m is a model                   
+                   var this_ = this;
+                   var m_id = m.id;                   
+                   if (this.models.indexOf(m) < 0) {
+                       // don't have the model yet, let's keep track of it in case it changes
+                       this.models.push(m);
+                       m.bind("change", function() {  this_.flush_model(m);  });
+                   }                   
+                   var result = v;
+                   this.cache[m_id] = this.cache[m_id] ? this.cache[m_id].filter(function(fnapp) { return fnapp[0] != fn; }).concat([fn, result]) : [[fn, result]];
+                   return result;
+               },
+               flush_model:function(m) {
+                   console.log('trigger on ', m, ' cache clearing ', this.cache[m.id].length , ' cached items');
+                   delete this.cache[m.id];
+               }
+           };
+           // ---------------------------------
 
+           var _chain_cache = new FnCache();
+           
            var chain = function(src_entity, target_type) {
                // forward chains from the src_entity to the target_type
                var rechain = arguments.callee;
                if (satisfies(src_entity, target_type)) { return [[]]; } // goal achieved.
-
                // find transforms that will give us our current destination
                var selected_Ts = TRANSFORMERS.filter(function(T) {  return satisfies(src_entity, T.domain); });
                var next = selected_Ts.map(function(T) {
-                   var val = T.fn(src_entity);
+                   var cached = _chain_cache.get_value(T,src_entity);
+                   if (cached) { return cached; }
+                   // not cached; proceeed                   
+                   var val = T.fn(src_entity); 
                    if (_(val).isArray()) {
                        var tails = _(val).filter(DEFINED).map(function(v) {
                            var chain_tail = rechain(v,target_type);
                            return chain_tail.map(function(tail_t) { return [[T,val]].concat(tail_t); });
                        });
-                       return flatten(tails);                       
+                       var result = flatten(tails);
+                       return _chain_cache.set_value(T,src_entity,result);
                    } else if (DEFINED(val)) {
 		       // we got a value directly -- let's propagate that on 
                        var chain_tail = rechain(val,target_type);
-                       return chain_tail.map(function(tail_t) { return [[T,val]].concat(tail_t); });
-                   } else {
-		       // not defined
-		       return undefined;
-		   }
-
-               }).filter(DEFINED);
-           
+                       var result =  chain_tail.map(function(tail_t) { return [[T,val]].concat(tail_t); });
+                       return _chain_cache.set_value(T,src_entity,result);
+                   }                   
+		   // not defined
+		   return undefined;
+               }).filter(DEFINED);           
                return flatten(next);
            };
 
