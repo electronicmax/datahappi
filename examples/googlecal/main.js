f2f406d525833dc0141abee140011e9af779f2ef
JimSkinner <js40g09@ecs.soton.ac.uk>
2012-07-10 12:39:58 +0100

    Modified getCal such that it works like RDFCollection.js, returning a collection with a working fetch function. Renamed getCal to CalendarCollection.

--- examples/googlecal/main.js
+++ examples/googlecal/main.js
@@ -1,11 +1,13 @@
 define(
-	['examples/googlecal/auth','examples/googlecal/getCal'],
+	['examples/googlecal/auth','examples/googlecal/CalendarCollection'],
 	function(authorize, calendar) {
 		gapi.client.setApiKey('AIzaSyAlspkJKDkbpqRkvZDED5xT9mjYVJXnUys');
 		$(document).ready(function() {
 			$('button#loginButton').click(function() {
 				authorize(function() {
-					calendar('jskinner.mail@gmail.com', function(d) {console.log(d);});
+					calendar.get_calendar().fetch().then(function(collection) {
+						window.c = collection;
+					});
 				});
 			});
 		});
