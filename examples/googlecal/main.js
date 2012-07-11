70b61e27baf2579d2efc7af0cad6f5572b5bfe80
Jim <js40g09@ecs.soton.ac.uk>
2012-07-11 15:46:36 +0100

    Added rdfquery imports to index. Addded importing of places and buildings to main.

--- examples/googlecal/main.js
+++ examples/googlecal/main.js
@@ -1,12 +1,16 @@
 define(
-	['examples/googlecal/auth','examples/googlecal/CalendarCollection','js/rdf/RDFCollection'],
-	function(authorize, calendar, rdfc) {
+	['examples/googlecal/auth','examples/googlecal/CalendarCollection','js/rdf/RDFCollection', 'js/ops/chain-engine'],
+	function(authorize, calendar, rdfc, ce) {
+		'strict mode';
 		gapi.client.setApiKey('AIzaSyAlspkJKDkbpqRkvZDED5xT9mjYVJXnUys');
 		$(document).ready(function() {
 			$('button#loginButton').click(function() {
 				authorize(function() {
-					calendar.get_calendar().fetch().then(function(collection) {
-						window.c = collection;
+					calendar.get_calendar().fetch().then(function(calCollection) {
+						window.c = calCollection;
+						rdfc.get_rdf('http://hip.cat/misc/rooms-and-buildings.rdf').fetch().then(function(eCollection) {
+							window.eCollection= eCollection;
+						});
 					});
 				});
 			});
