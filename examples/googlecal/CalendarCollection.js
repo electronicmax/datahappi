d714059cde780e1c09c4aeccdb0a9b6c1c4aa455
electronic max <electronic@gmail.com>
2012-07-11 15:06:04 +0100

    small change to calendar objects

--- examples/googlecal/CalendarCollection.js
+++ examples/googlecal/CalendarCollection.js
@@ -9,9 +9,24 @@ define(
 			fetch:function() {
 				var that = this;
 				var deferred = $.Deferred();
+                                var _convert_val = function(v,k) {
+                                    if (!_(v).isArray()) { v = [v]; }
+                                    return v.map(function(vv) {
+                                        // value specific coercion here e.g., date time formats to our date time format
+                                        return vv;
+                                    });
+                                };
+                                var _convert_event = function(evt) {
+                                    // converts an entire event
+                                    var new_evt = {};
+                                    _(evt).keys().map(function(k) {
+                                        new_evt[k] = _convert_val(evt[k],k);                                        
+                                    });
+                                    return new_evt;
+                                };                            
 				gapi.client.load('calendar', 'v3', function() {
 					gapi.client.calendar.events.list({calendarId:that._id}).execute(function(eventList) {
-						that.reset(eventList.items);
+						that.reset(eventList.items.map(function(evt) { return _convert_event(evt); }));
 						deferred.resolve(that);
 					});
 				});
