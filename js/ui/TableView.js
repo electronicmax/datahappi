4f4831c98ca309d3364cccc7201a48e7910afb80
Jim <js40g09@ecs.soton.ac.uk>
2012-07-12 18:31:25 +0100

    Strengthened checking in TableView to see if val is a single value array.

--- js/ui/TableView.js
+++ js/ui/TableView.js
@@ -20,12 +20,12 @@ define(['js/utils'], function(util) {
 			var this_ = this;
 			_(this.options.columns).map(
 				function(val,property) {
-					if (_.isUndefined(val)) { val = '<i>undefined</i>'; console.log(val); }
+					if (_.isUndefined(val)) { val = '<i>undefined</i>'; }
 					if (_(val).isFunction()) { val = val(m); }
 					if (_(val).isNumber()) { val = val.toString(); }
 					if (_(val).isObject() && val instanceof Backbone.Model) { val = val.attributes._id || val.attibutes._oid; }
 					if (_(val).isObject() && !_(val).isElement()) { return "object"; }
-					if (val.length === 1) {val = val[0];}
+					if (_.isArray(val) && val.length === 1) {val = val[0];}
 					if (_(val).isString() || _(val).isElement()) {
 						$("<td></td>").append(val).appendTo(this_.el);
 					} else {
