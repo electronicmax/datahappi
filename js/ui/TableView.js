c2d5218f189a939c7b35201cc45e7b3797c7d872
Jim <js40g09@ecs.soton.ac.uk>
2012-07-13 13:49:43 +0100

    In column rendering, moved checkign for single element array below checking for function; chaining to place names now appears to work correctly, yay!

--- js/ui/TableView.js
+++ js/ui/TableView.js
@@ -20,13 +20,14 @@ define(['js/utils'], function(util) {
 			var this_ = this;
 			_(this.options.columns).map(
 				function(val,property) {
-					if (_.isArray(val) && val.length === 1) {val = val[0];} //If val is a single vlaue array, take the value out of the array.
+					oldVal = $.extend({}, val);
 					if (_(val).isFunction()) { val = val(m); }
+					if (_.isArray(val) && val.length === 1) {val = val[0];} //If val is a single vlaue array, take the value out of the array.
 					if (_.isUndefined(val)) { val = '<i>undefined</i>'; }
 					if (_(val).isNumber()) { val = val.toString(); }
 					if (_(val).isObject() && val instanceof Backbone.Model) { val = val.attributes._id || val.attibutes._oid; }
 					//if (_(val).isObject() && !_(val).isElement()) { return "object"; } // return if val is an object, but not if it is an element object
-					if (_(val).isObject() && !_(val).isElement()) { val = "non-element object"; } // return if val is an object, but not if it is an element object
+					if (_(val).isObject() && !_(val).isElement()) { console.log(oldVal); val = "non-element object"; } // return if val is an object, but not if it is an element object
 					if (_(val).isString() || _(val).isElement()) { // jQuery can handle html strings and element objects.
 						$("<td></td>").append(val).appendTo(this_.el);
 					} else {
@@ -64,7 +65,7 @@ define(['js/utils'], function(util) {
 						if (names && names.length > 0) {
 							return names[0].get('place name');
 						} else {
-							return ':(';
+							return 'could not chain to lat/long or place name';
 						}
 					}
 					return ' ? ';
