3b857ffcd4504f01b1947cd492aa557527cf8937
electronic max <electronic@gmail.com>
2012-07-15 20:27:13 +0100

    box view first attempt

---
+++ css/compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true; do
+	echo "compilin'"
+	lessc box.less > box.css
+	sleep 1
+done
---
+++ js/box.js
@@ -0,0 +1,51 @@
+define([], function() {
+
+	var template = '<div class="uplt"></div><div class="uprt"></div><div class="btlt"></div><div class="btrt"></div><div class="items"></div><input type="text" value="<%= label %>"></input>';
+	var clone_view = function(src_view) {
+		// makes a new view from the same model as src_model
+		var view = new src_view.constructor(src_view.options);
+		console.log(">>> clone ", view, src_view, src_view.constructor);
+		window.srcview = src_view;
+		view.render();
+		return view;
+	};	
+	var BoxView = Backbone.View.extend({
+		template:template,
+		tagName:'div',
+		className:'greybox',
+		initialize:function() {
+			this.options.views_collection = this.options.views_collection ? this.options.views_collection : new Backbone.Collection();
+			this.render();
+		},
+		render:function() {
+			var this_ = this;
+			this.$el.html(_(this.template).template({label:this.options.label || 'stuff'}));
+			this.$el.draggable();
+			this.options.views_collection.map(function(v) { this_._add_view(v); });
+			this.$el.droppable({
+				accept:'.item, .simple',
+				tolerance:"touch",
+				over:function(event, ui) {
+					$(this).addClass("over");
+				},
+				out:function(event, ui) {
+					$(this).removeClass("over");
+				},				
+				drop: function( event, ui ) {
+					$(this).removeClass("over");
+					var view = clone_view(ui.draggable.data("view"));
+					this_.add(view);
+				}
+			});
+			return this.el;
+		},
+		_add_view:function(v) {
+			this.$el.find('.items').append(v.render());
+		},
+		add:function(v) {
+			this.options.views_collection.add(v);
+			this._add_view(v);
+		}
+	});
+	return { BoxView : BoxView };
+});
