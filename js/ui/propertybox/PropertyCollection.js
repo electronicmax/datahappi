b6e134a176657208616d1440a63912b8840b663d
electronic max <electronic@gmail.com>
2012-07-17 13:08:41 +0100

    moved things into jim's framework

--- js/ui/propertybox/PropertyCollection.js
+++ js/ui/propertybox/PropertyCollection.js
@@ -1,8 +1,22 @@
 define(
-	['js/models/PropertyModel'],
-	function(PropertyModel) {
+	[
+		'js/models/PropertyModel',
+		'js/utils'				
+	],
+	function(PropertyModel, util) {
 		PropertyCollection = Backbone.Collection.extend({
 			model:PropertyModel
+			initialize:function() {
+				var this_ = this;
+				util.assert(this.options.collection, "No collection passed");
+				dthis.prop_collection = new Backbone.Collection();
+				this.options.collection.bind('change', function() { this_._changed(); });				
+			},
+			_changed:function(){
+				// TODO :: iterates over all of the objects in our collection
+				// finds our properties and adds nice Models representing each
+				// of them to us.				
+			}
 		});
 		return PropertyCollection;
 	}
--- js/ui/propertybox/propertyview.js
+++ js/ui/propertybox/propertyview.js
@@ -1,35 +1,21 @@
 /* this is for the box example only  */
 define(
 	[
+		'js/ui/propertybox/PropertyModel',
+		'js/ui/propertybox/PropertyCollection',
 		'js/draggableview'
-		'js/utils'
 	],
-	function(dv,util) {
-		var template="<div></div>"; // TODO
-		var PropertyCollection = Backbone.Collection.extend({
-			initialize:function() {
-				var this_ = this;
-				util.assert(this.options.collection, "No collection passed");
-				dthis.prop_collection = new Backbone.Collection();
-				this.options.collection.bind('change', function() { this_._changed(); });				
-			},
-			_changed:function(){
-				// TODO :: iterates over all of the objects in our collection
-				// finds our properties and adds nice Models representing each
-				// of them to us.				
-			}
-		});
-
+	function(PropertyModel, PropertyCollection, dv) {
 		var PropertyView = Backbone.View.extend({
 			// paints a nice property.
 			template:"<div></div>",
 			render:function() {
 				return this.el;
 			}
-		});
-		
+		});		
 		var PropertyBox = Backbone.View.extend({
 			/* @requires: src 'collection' of models to generate properties for -- passed in to options  */
+			template:"<div></div>", // TODO.
 			initialize:function() {
 				var this_ = this;
 				this.collection = new PropertyCollection(this.options.collection);
