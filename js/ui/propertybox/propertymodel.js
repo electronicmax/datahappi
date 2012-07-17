528a61848d1f0ec775e7eabf85193be6b058b28d
electronic max <electronic@gmail.com>
2012-07-17 18:10:01 +0100

    closer to getting property slidey

--- js/ui/propertybox/propertymodel.js
+++ js/ui/propertybox/propertymodel.js
@@ -20,7 +20,7 @@ define([],function() {
 				});
 				var coverage = models_that_have_us.length * 1.0/this.collection.length;
 				this.set({'coverage': coverage});
-			}
+			},
 			_update_entropy:function() {
 				// find all models in our collection that have us.
 				var values_of_us = this.collection.filter(function(m) {
