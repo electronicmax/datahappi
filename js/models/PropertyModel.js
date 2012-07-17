09f0f5c61784b8576293f8e1259d02cd01884ff4
Jim <js40g09@ecs.soton.ac.uk>
2012-07-17 12:47:30 +0100

    Added PropertyCollection and PropertyModel, each in respective 'collections' and 'models' folders.

---
+++ js/models/PropertyModel.js
@@ -0,0 +1,15 @@
+define(
+	[],
+	function() {
+		PropertyModel = Backbone.Model.extend({
+			converage: function() {
+				return 0;
+			},
+			entropy: function() {
+				return 0;
+			}
+		});
+		return PropertyModel;
+	}
+);
+
