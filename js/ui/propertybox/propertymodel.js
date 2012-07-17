2c9a06cbdf5c2fa16638348d3fba59b3ad3bcb68
Jim <js40g09@ecs.soton.ac.uk>
2012-07-17 14:50:53 +0100

    Added skeletal initialize funtion.

--- js/ui/propertybox/propertymodel.js
+++ js/ui/propertybox/propertymodel.js
@@ -1,7 +1,12 @@
 define(
 	[],
 	function() {
-		PropertyModel = Backbone.Model.extend({
+		var PropertyModel = Backbone.Model.extend({
+			idAttribute:"_id",
+			initialize:function() {
+				// make sure that this.attributes._id is the property name
+				this.name = this.attributes._id;
+			},
 			converage: function() {
 				return 0;
 			},
