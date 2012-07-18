c2cf6a3c7fe0e6c654f74b8403e824bf7779c03d
Jim <js40g09@ecs.soton.ac.uk>
2012-07-18 11:11:50 +0100

    Added skeleton for the display to graph selected properties.

---
+++ js/ui/displaybox/displayview.js
@@ -0,0 +1,20 @@
+define(
+	[
+		'js/ui/displaybox/displaycollection',
+		'js/draggableview'
+	],
+	function(dc, dv) {
+		var DisplayView = Backbone.View.extend({
+
+		});
+
+		var DisplayBox = Backbone.View.extend({
+
+		});
+
+		return {
+			DisplayBox:DisplayBox
+		};
+	}
+);
+
