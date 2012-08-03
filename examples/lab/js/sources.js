6d9a460e9fb7b18acf7baf6c121f350ee90aaf89
electronic max <electronic@gmail.com>
2012-08-03 21:48:07 +0100

    finished minor refactoring of source class - must do moar for json dudes

--- examples/lab/js/sources.js
+++ examples/lab/js/sources.js
@@ -2,6 +2,8 @@ define(['examples/lab/js/pathables','js/utils'],  function(pathables,util) {
 	var Source = Backbone.Model.extend({
 		idAttribute: "name",
 		defaults: { name: "Things", url: "" },
+	});
+	var RDFSource = Source.extend({
 		fetch:function() {
 			var this_ = this;
 			var d = util.deferred();
@@ -11,8 +13,15 @@ define(['examples/lab/js/pathables','js/utils'],  function(pathables,util) {
 				d.resolve(c);
 			});
 			return d.promise(); 
-		}
-	});		
+		}		
+	});
+	var JSONSource = Source.extend({
+		fetch:function() {
+			var this_ = this;
+			var d = util.deferred();
+			throw new Error("Can't do yet");
+		}		
+	});	
 	var SourceView = Backbone.View.extend({
 		tagName: "li",
 		className: "source",
@@ -21,9 +30,17 @@ define(['examples/lab/js/pathables','js/utils'],  function(pathables,util) {
 	var SourceCollection = Backbone.Collection.extend({
 		model:Source
 	});
+	var get_source = function(name, src_url) {
+		var endswith = function(s, suffix) { return s.indexOf(suffix) >= 0 && s.indexOf(suffix) + suffix.length == s.length; }
+		if (endswith(src_url, '.rdf')) { return new RDFSource({name:name, url:src_url}); }
+		if (endswith(src_url, '.json')) { return new JSONSource({name:name, url:src_url}); }
+		throw new Error("can't handle data of type ", src_url);
+	};
 	return {
 		Source:Source,
+		RDFSource:RDFSource,
 		SourceView:SourceView,
-		SourceCollection:SourceCollection
+		SourceCollection:SourceCollection,
+		get_source:get_source
 	};
 });
