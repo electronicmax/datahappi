e09f44b3679f991c493b4ca96d73557846178b17
Jim <js40g09@ecs.soton.ac.uk>
2012-08-06 18:59:56 +0100

    Fixed a bug when trying to path on a property whch has no entailed properties.

--- examples/lab/js/propertyview.js
+++ examples/lab/js/propertyview.js
@@ -113,7 +113,7 @@ define(
 			},
 			_pathable_add:function(pathable) {
 				var this_ = this;
-				_.union(_.keys(pathable.entailed), _.keys(pathable.attributes)).filter(function(attribute) {
+				_.union(_.keys(pathable.entailed || {}), _.keys(pathable.attributes)).filter(function(attribute) {
 					return !(_.isUndefined(attribute) || attribute === "_id");
 				}).map(function(attribute) {
 					var model_attributes = {};
