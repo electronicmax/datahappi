d7f83f3f844b70570d6ad4f9ac1b62bae4c27e26
electronic max <electronic@gmail.com>
2012-08-07 02:35:54 +0100

    mostly there for deletes supprot

--- examples/lab/js/instancebox.js
+++ examples/lab/js/instancebox.js
@@ -8,7 +8,7 @@ define(
 		'js/utils'
 	],
 	function(box, propbox, pathbox, pathables, pathableview, utils) {
-		var template = '<div class="uplt"></div><div class="uprt"></div><div class="btlt"></div><div class="btrt"></div><div class="items"></div><input type="text" value="<%= label %>"></input>';
+		var template = '<div class="box-delete icon-cancel"></div><div class="uplt"></div><div class="uprt"></div><div class="btlt"></div><div class="btrt"></div><div class="items"></div><input type="text" value="<%= label %>"></input>';
 		var toolbar_template = '<div class="microtoolbox"><span class="toggle_paths"></span><span class="toggle_props icon-logout"></span></div><div class="properties"></div>';
 		var defined = utils.DEFINED;
 		
