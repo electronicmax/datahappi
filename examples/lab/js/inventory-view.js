b415b3821da2f4e9d9ab08d9fbd75f3c6c28ec22
electronic max <electronic@gmail.com>
2012-08-23 18:00:43 +0100

    inventory view

--- examples/lab/js/inventory-view.js
+++ examples/lab/js/inventory-view.js
@@ -8,7 +8,7 @@ define(['examples/lab/js/views','js/utils'],function(views,utils) {
 		},
 		template:'',
 		category_template:"<div class='subcollection'><div class='lbl'><%= label %></div><div class='items'></div></div>",
-		thinglens: 
+		thinglens: views.ThingListItemView,
 		initialize:function() {
 			var this_ = this;
 			this.collection = this.options.collection || new Backbone.Collection();
@@ -21,60 +21,72 @@ define(['examples/lab/js/views','js/utils'],function(views,utils) {
 		},
 		render:function() {
 			console.log('render!! ', this.$el, this.collection.length);
-			var this_ = this;			
-			var visible_dudes = this._get_visible_dudes();
-			console.log('visible_dudes ', visible_dudes.length);
-			// enter selection
-			var types = _.uniq(flatten(visible_dudes.map(function(x) {
-				return x.model.get('type') || [];
-			})));
-			var get_subcollection_div = function(typeid) {
-				return this_.$el.find('div.subcollection[class-uri="'+typeid+'"]');
+			var this_ = this;
+			var id_of_pathable = function(pathable) { return pathable.model.id; };
+			var ids_of_elements = function($sel) {
+				return $.makeArray($sel.map(function() {
+					return id_of_pathable( $(this).data('view').options.model ) ;
+				}));
 			};
+			var get_subcollection_for_type = function(typeid) {
+				return this_.$el.find('div.subcollection[class-uri="'+typeid+'"]');
+			};			
+			var elements_with_ids = function($el, ids) {
+				return $.makeArray($el.filter(function() {
+					return ids.indexOf(id_of_pathable( $(this).data('view').options.model )) >= 0;
+				}));
+			};
+			
+			var visible_dudes = this._get_visible_dudes();
+			var dude_types = _.uniq(flatten(visible_dudes.map(function(x) {
+				return x.model.get('type') || [];
+			}))).concat(['__misc__']);
+			
 			// subcollection enter selection
-			types.map(function(type) {
-				if (get_subcollection_div(type.id).length == 0) {
+			dude_types.map(function(type) {
+				console.log('type ', type);
+				if (type == '__misc__') {
+					if (get_subcollection_for_type('__misc__').length == 0) {
+						console.log('populating > ', _(this_.category_template).template({label:'other'}));
+						$(_(this_.category_template).template({label:'other'}))
+							.attr('class-uri', '__misc__')
+							.appendTo(this_.$el);
+					}
+					return;
+				}
+				if (get_subcollection_for_type(type.id).length == 0) {
 					$(_(this_.category_template).template({label:type.get_label()}))
 						.attr('class-uri', type.id)
 						.appendTo(this_.$el);
 				}
 			});
-			console.log('types :< >> ', types);
 			
 			// subcollection exit selection
 			var exit_sel_ids = _(this_.$el.find('div.subcollection').map(function(x) {
 				return $(this).attr('class-uri');
-			})).difference(types.map(function(x) { return x.id; }));
-
-			this_.$el.find('div.subcollection')
-				.filter(function() { return exit_sel_ids.indexOf($(this).attr('class-uri')) >= 0; })
-				.remove();
+			})).difference(dude_types.map(function(x) { return x.id; }).concat('__misc__'));
 			
+			this.$el.find('div.subcollection')
+				.filter(function() { return exit_sel_ids.indexOf($(this).attr('class-uri')) >= 0; })
+				.remove();			
+
 			// subcollection populate
-			var id_of_pathable = function(pathable) { return pathable.model.id; };
-			var ids_of_elements = function($sel) {
-				return $sel.map(function() {
-					return id_of_pathable( $(this).data('view').options.model ) ;
-				});
-			};
-			var elements_with_ids = function($el, ids) {
-				return $el.filter(function() {
-					return ids.indexOf(id_of_pathable( $(this).data('view').options.model )) >= 0;
-				});
-			};
 			this_.$el.find('div.subcollection').map(function() {
 				var this_subcollection = $(this);
 				var subcollection_id = $(this).attr('class-uri');
 				var relevant_things = visible_dudes.filter(function(x) {
-					return (x.model.get('type') || []).map(function(x) { return x.id; })
-						.indexOf(subcollection_id) >= 0;
+					if (subcollection_id == '__misc__') {
+						return x.model.get('type') == undefined || x.model.get('type').length === 0; 
+					}
+					return (x.model.get('type') || []).map(function(x) { return x.id; }).indexOf(subcollection_id) >= 0;
 				});
 				var relevant_ids = relevant_things.map(id_of_pathable);
 				
 				// TODO TODO TODO -------------------------------------------------------------------
 				// enter selection
- 				var enter_ids = _(relevant_ids)
-					.difference(ids_of_elements($(this).find('.item')));
+				console.log("IDS OF ELEMENTS ", relevant_ids, 'vs', ids_of_elements($(this).find('.item')));
+ 				var enter_ids = _(relevant_ids).difference(ids_of_elements($(this).find('.item')));
+				console.log("ENTER IDs ", enter_ids);
 
 				// create for visible dudes
 				visible_dudes.filter(function(pathable) {
@@ -84,12 +96,11 @@ define(['examples/lab/js/views','js/utils'],function(views,utils) {
 					this_subcollection.find('.items').append(lens.el);
 					lens.$el.attr('uri', id_of_pathable(pathable));
 				});
-
+				
 				// exit selection
-				elements_with_ids(
-					_(ids_of_elements($(this).find('.item'))).difference(relevant_ids)
-				).remove();
-
+				var exit_ids = _(ids_of_elements($(this).find('.item'))).difference(relevant_ids);
+				console.log(' exit ids ', exit_ids, ' - ', elements_with_ids(exit_ids));
+				$(elements_with_ids(exit_ids)).remove();
 				// update selection? no need -
 			});
 			return this;
