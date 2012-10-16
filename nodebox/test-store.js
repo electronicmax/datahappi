5b30278a277db1bcf2b628a64d768d940254d2a4
electronic max <electronic@gmail.com>
2012-10-16 01:02:33 +0100

    such delicious people

--- nodebox/test-store.js
+++ nodebox/test-store.js
@@ -4,17 +4,27 @@ var ns = require('nodebox/store'),
     log = require('nlogger').logger(module);
 
 var s = new ns.Store();
+var connect = function() {
+	return s.connect();
+};
 
-s.connect().then(function() {
-	console.log('ready to party');
+var test_write_random = function() {
+	var model = m.DEFAULT_GRAPH.get_or_create('http://hip.cat/emax-'+(new Date().valueOf()));
+	log.debug(' got model ' +  model.id + ' graph >> ', model.graph.id);
+	model.set('loves', m.DEFAULT_GRAPH.get_or_create('http://hip.cat/melkins'));
+	s.write(model).then(function() {
+		console.log("WON!");
+	}).fail(function(err) { console.log("FAIL", err); });
+};
+
+if (require.main === module) {
+	s.connect().then(function() {
+		console.log('READY TO ROCK >> ready to party');
        	s.create_tables().then(function() {
-		console.log('tables exist, ready to go ');
-		var model = m.DEFAULT_GRAPH.get_or_create('http://hip.cat/emax-'+(new Date().valueOf()));
-		log.debug(' got model ' +  model.id + ' graph >> ', model.graph.id);
-		model.set('loves', m.DEFAULT_GRAPH.get_or_create('http://hip.cat/melkins'));
-		s.write(model).then(function() {
-			console.log("WON!");
-		}).fail(function(err) { console.log("FAIL", err); });
+			console.log('tables exist, ready to go ');
+			test_write_random();
+		});
 	});
-});
-
+}  else {
+	s.connect().then(function() { console.log("Connected and READY TO ROCK "); });
+}
